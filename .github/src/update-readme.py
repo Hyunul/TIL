81b76c8b48c9d541cdd4028b160436383b32e501
Hyunul <jungwj1023@naver.com>
2023-05-08 15:38:54 +0900

    'test'

--- .github/src/update-readme.py
+++ .github/src/update-readme.py
@@ -102,7 +102,8 @@ with open("README.md", "w") as f:
     file_list.sort(key=lambda file: file[2], reverse=True)
     f.write("### Categories\n")
     for file in file_list:
-        f.write("✔[{}](#{})\n".format(file[0], file[0]))
+        # file[0] = ./algorithm/18.py
+        f.write("✔[{}](#{})\n".format(file[0].replace(".", ""), file[0]))
     f.write("\n")
 
     for file in file_list:
